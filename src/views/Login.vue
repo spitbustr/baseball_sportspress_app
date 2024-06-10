9f7202037bf9680c2d4b0a5ca286cf8cc1489b9c
Daniel Brousseau <brousseau.d@gmail.com>
2024-06-10 09:49:13 -0400

    Login change

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,17 @@
 <template>
-    <h1>Grosse  page de login</h1>
-    <div>
-        <div>
-            <label for="username">Username</label>
-            <input id="username" v-model="payload.username"/>
+    <div class="text-center">
+        <h1>Grosse page de login</h1>
+        <div class="mt-3">
+            <div>
+                <label for="username">Username</label><br>
+                <input id="username" v-model="payload.username" />
+            </div>
+            <div class="mt-3">
+                <label for="password">Password</label><br>
+                <input id="password" type="password" v-model="payload.password">
+            </div>
+            <button @click="login()" class="mt-3">LOGIN</button>
         </div>
-        <div>
-            <label for="password">Password</label>
-            <input id="password" v-model="payload.password">
-        </div>
-        <button @click="login()">LOGIN</button>
     </div>
 </template>
 <script>
@@ -19,17 +21,17 @@ export default {
     data() {
         return {
             payload: {
-                username: "adminuser",
-                password: "LBAO4ever!",
+                username: "",
+                password: "",
             }
         }
     },
     methods: {
         async login() {
             const result = await UserService.loginUser(this.payload)
-            if(result.data.id && result.status === 200) {
-                this.$store.dispatch("user/setUser",result.data)
-                this.$store.dispatch("user/setLoggedIn",true)
+            if (result.data.id && result.status === 200) {
+                this.$store.dispatch("user/setUser", result.data)
+                this.$store.dispatch("user/setLoggedIn", true)
                 this.$router.push({ name: 'home' })
             }
         }
